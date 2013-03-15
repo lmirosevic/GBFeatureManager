1904692f0c24efdb52323882cba0a6c371b061ca
Luka Mirosevic <luka@goonbee.com>
2013-03-15 12:25:31 +0100

    Initial Commit

---
+++ GBFeatureManager/GBFeatureManager.h
@@ -0,0 +1,13 @@
+//
+//  GBFeatureManager.h
+//  GBFeatureManager
+//
+//  Created by Luka Mirosevic on 15/03/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface GBFeatureManager : NSObject
+
+@end
---
+++ GBFeatureManager/GBFeatureManager.m
@@ -0,0 +1,13 @@
+//
+//  GBFeatureManager.m
+//  GBFeatureManager
+//
+//  Created by Luka Mirosevic on 15/03/2013.
+//  Copyright (c) 2013 Goonbee. All rights reserved.
+//
+
+#import "GBFeatureManager.h"
+
+@implementation GBFeatureManager
+
+@end
